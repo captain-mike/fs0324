bb31bdad90873e9091318af0087dd45f46aa1440
captain-mike <info@grafikweb.it>
2024-06-04 13:07:46 +0200

    commenti

--- m3/s3/g2/reactive-forms/src/app/reactive-form/reactive-form.component.ts
+++ m3/s3/g2/reactive-forms/src/app/reactive-form/reactive-form.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-form',
@@ -19,11 +19,13 @@ export class ReactiveFormComponent {
       cognome: this.fb.control(null),//il campo cognome
       authData: this.fb.group({//il form group chiamato authData
         email:this.fb.control(null,[
-          Validators.required,//validatori sincroni
-          Validators.email//validatori sincroni
-        ]),
+          Validators.required,//validatore sincrono
+          Validators.email,//validatore sincrono
+          this.emailProibiteValidator//validatore sincrono
+        ], this.emailEsistente),//validatore asincrono
         password:this.fb.control(null)
-      })
+      }),
+      sports: this.fb.array([])//un array di campi, vuoto ma ne aggiungeremo successivamente
     })
 
   }
@@ -39,7 +41,66 @@ export class ReactiveFormComponent {
     return field?.invalid && field?.touched//Verifico se il campo è valido e se è stato anche toccato
   }
 
+  getMessage(fieldName: string) {
+    return this.form.get(fieldName)?.errors!['message']//restituisce il messaggio di errore dei custom validators
+  }
 
+
+/**
+ * metodi per la gestione dei campi sport
+ */
+addSport(){
+  let control = new FormControl(null);//creo un campo per il form
+  (this.form.get('sports') as FormArray).push(control);//accedo all'array sports della riga 31 e aggiungo il campo appena creato
+}
+
+getSports(){
+  return (this.form.get('sports') as FormArray).controls;//restituisce tutti i campi nell'array sports
+}
+
+
+
+  emailProibite:string[] = ['mario@rossi.it','giampaolo@paladino.it'];
+
+//validatore custom sincrono
+  //verifica l'inserimento di e-mails presenti nell'array della riga 63
+  emailProibiteValidator = (formC:FormControl):ValidationErrors|null =>{
+
+    if(this.emailProibite.includes(formC.value)){//se la mail inserita dall'utente viene trovata anche nell'array della riga 63
+
+      return {//restituisce un oggetto contenente dettagli della validazione
+        invalid:true,
+        message:'Non può entrareeh!'
+      }
+
+    }
+
+    return null//altrimenti restituisce null
+
+  }
+
+  //validatore asincrono
+  //simula la richiesta al server per verificare se la mail inserita è già in uso
+  emailEsistente(formC:AbstractControl){
+    return new Promise<ValidationErrors|null>((resolve, reject)=>{//con la promise possiamo rendere asincrona l'esecuzione
+
+      setTimeout(()=>{//simuliamo l'attesa dovuta ad una chiamata ajax
+
+        if(formC.value == 'admin@admin.it'){//creo una situazione in cui la validazione si attiva
+
+          resolve({//in caso di successo invio i dettagli dell'errore
+            invalid:true,
+            message:'Non può entrareeh!'
+          })
+
+        }
+
+        reject(null)//se la validazione non si attiva invio null
+
+      },2000)
+
+    })
+  }
 
 
 }
